f71f0a13b2dfe88784de01a7e1d0de12f6a10150
victoriavaskova <victoriavaskova@gmail.com>
2025-03-12 16:53:58 +0300

    app routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router";
+import Layout from "./widgets/Layout/Layout";
+import MainPage from "./pages/MainPage/MainPage";
+import LoginPage from "./pages/loginPage/loginPage";
+import CreateSockPage from "./pages/createSockPage/createSockPage"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    <div> </div>
-    </>
-  )
+const [user, setUser] = useState({ status: "logging", data: null });
+return (
+  <>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/createsock" element={<CreateSockPage />} />
+        // <Route path="/signup" element={<SignUpPage setUser={setUser}/>} />
+        <Route path="/login" element={<LoginPage setUser={setUser}/>} />
+        <Route path="*" element={<h1>No content</h1>} />
+      </Route>
+    </Routes>
+  </>
+);
 }
-
 export default App
