9d06bf0bf8c8e9512539b689f5e5cf8208c1066c
SrgChuban <19ss4@mail.ru>
2025-03-14 13:50:37 +0500

    cart logics done

--- client/src/features/CartForm/CartForm.jsx
+++ client/src/features/CartForm/CartForm.jsx
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import  './cartStyles.css'; 
+import axiosInstance from "../../shared/lib/axiosinstance";
 
 const CartForm = () => {
   const [quantity, setQuantity] = useState(1);
+  const [socks, setSocks] = useState([])
   const pricePerItem = 1000;
   const totalPrice = quantity * pricePerItem;
+  
+  useEffect(() => {
+    try {
+      axiosInstance("/cart").then((res) => {
+        setSocks(res.data.allSocks);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [])
 
   const handleIncrement = () => {
     setQuantity(quantity + 1);
@@ -16,17 +28,26 @@ const CartForm = () => {
     }
   };
 
+  
+
+
   return (
-    
+    <>
     <div className="cart-container">
       <div className="header">
         <h1>Корзина</h1>
       </div>
-      <div className="content">
-        <div className="image-container">
-          <img src="/public/red.png" alt="sock_Img" />
+      {socks.map((el) => (
+        <div key={el.id} className='content'>
+          <div className="image-container">
+          <img src={el.Sock.image} alt={el.Sock.color} />
         </div>
-        <div className="counter">
+        <div className='sock-info'>
+          <p>Цвет: {el.Sock.color}</p>
+          <p>Узор: {el.Sock.pattern}</p>
+          </div>
+
+          <div className="counter">
           <button onClick={handleDecrement}>-</button>
           <span>{quantity}</span>
           <button onClick={handleIncrement}>+</button>
@@ -34,14 +55,22 @@ const CartForm = () => {
         <div className="price-info">
           <div className="price">Цена: ${pricePerItem}</div>
         </div>
-      </div>
-      <div className="order-button-div">
+
+        </div>
         
+        )
+      )}
+      
+        
+    
+      
+      <div className="order-button-div">
       </div>
+
       <div className="total">ИТОГО: ${totalPrice}</div>
       <button className="order-button">Заказать</button>
     </div>
-    
+    </>
   );
 };
 
--- server/src/routers/cartRouter.js
+++ server/src/routers/cartRouter.js
@@ -1,24 +1,45 @@
 const cartRouter = require('express').Router();
-const { User, Socks, Cart }  = require('../../db/models/');
-const { verifyAccessToken, verifyRefreshToken }  = require('../middlewares/VerifyTokens');
+const { User, Socks, Cart } = require('../../db/models/');
+const { verifyAccessToken, verifyRefreshToken } = require('../middlewares/VerifyTokens');
 
 cartRouter.post('/', verifyAccessToken, async (req, res) => {
-    try {
-      const userId = res.locals.user.id; 
-      const { sockId } = req.body; 
-  
-      if (!sockId) {
-        return res.status(400).json({ error: 'Не указан sockId' });
-      }
-  
-      const newCartItem = await Cart.create({ userId, sockId });
-  
-      res.json({ message: 'Носок добавлен в корзину', cartItem: newCartItem });
-    } catch (error) {
-      console.error('Ошибка при добавлении в корзину:', error);
-      res.status(500).json({ error: 'Ошибка сервера' });
-    }
-  });
-  
-  module.exports = cartRouter;
+  try {
+    const userId = res.locals.user.id;
+    const { sockId } = req.body;
 
+    if (!sockId) {
+      return res.status(400).json({ error: 'Не указан sockId' });
+    }
+
+    const newCartItem = await Cart.create({ userId, sockId });
+
+    res.json({ message: 'Носок добавлен в корзину', cartItem: newCartItem });
+  } catch (error) {
+    console.error('Ошибка при добавлении в корзину:', error);
+    res.status(500).json({ error: 'Ошибка сервера' });
+  }
+});
+
+cartRouter.get('/', verifyAccessToken, async (req, res) => {
+  try {
+    const userId = res.locals.user.id;
+
+    if (!userId) {
+      return res.status(400).json({ error: 'Не указан userId' });
+    }
+
+    const allSocks = await Cart.findAll({
+      where: { userId },
+      include: [{model: Socks}],
+    })
+
+    res.json({allSocks})
+    console.log('Получены носки!', allSocks);
+    
+  } catch (error) {
+    console.error('Ошибка при добавлении в корзину:', error);
+    res.status(500).json({ error: 'Ошибка сервера' });
+  }
+});
+
+module.exports = cartRouter;
