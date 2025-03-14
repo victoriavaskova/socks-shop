175d91d64f8b39a0be166b138550f52881c1f598
victoriavaskova <victoriavaskova@gmail.com>
2025-03-14 14:55:45 +0300

    footer+auth error

---
+++ client/src/widgets/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>Адрес: г. Москва, ул. Орджоникидзе, 11к11</p>
+        <p>Email: <a href="mailto:yourfavoritesocks@support.com">yourfavoritesocks@support.com</a></p>
+      </div>
+    </footer>
+  );
+}
